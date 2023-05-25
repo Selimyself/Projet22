d3f8714e7becf1bb756d331e22c72386b632a2f0
selimyasar <selimyasar772@gmail.com>
2023-05-25 16:44:40 +0300

    AccountController

---
+++ app/controllers/AccountController.js
@@ -0,0 +1,49 @@
+const Account = require('../models/Account');
+
+// Créer un compte
+exports.createAccount = async (req, res) => {
+  try {
+    const { userId, bankName, accountName, lastUpdate } = req.body;
+
+    const newAccount = await Account.create({ userId, bankName, accountName, lastUpdate });
+
+    res.status(201).json(newAccount);
+  } catch (error) {
+    res.status(500).json({ message: 'Une erreur s\'est produite lors de la création du compte.' });
+  }
+};
+
+// Mettre à jour un compte
+exports.updateAccount = async (req, res) => {
+  try {
+    const accountId = req.params.id;
+    const { bankName, accountName, lastUpdate } = req.body;
+
+    const updatedAccount = await Account.findByIdAndUpdate(accountId, { bankName, accountName, lastUpdate }, { new: true });
+
+    if (!updatedAccount) {
+      return res.status(404).json({ message: 'Compte non trouvé.' });
+    }
+
+    res.status(200).json(updatedAccount);
+  } catch (error) {
+    res.status(500).json({ message: 'Une erreur s\'est produite lors de la mise à jour du compte.' });
+  }
+};
+
+// Supprimer un compte
+exports.deleteAccount = async (req, res) => {
+  try {
+    const accountId = req.params.id;
+
+    const deletedAccount = await Account.findByIdAndRemove(accountId);
+
+    if (!deletedAccount) {
+      return res.status(404).json({ message: 'Compte non trouvé.' });
+    }
+
+    res.status(200).json({ message: 'Compte supprimé avec succès.' });
+  } catch (error) {
+    res.status(500).json({ message: 'Une erreur s\'est produite lors de la suppression du compte.' });
+  }
+};
